6b38d0aad61f85ea43a32ec35801590f5a2530c9
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-08-06 22:07:48 +0300

    changes

---
+++ public/product_page/script.js
@@ -0,0 +1,93 @@
+function getProducts() {
+    // const container = document.getElementById("container");
+    fetch("https://fakestoreapi.com/products")
+    .then(res=> {
+        console.log(res);
+        return res.json()})
+    .then(data=>{
+        console.log(data)
+        // filterItems(data)})
+        loadProducts(data)})
+        // loadProducts(data)
+    .catch(e=>{
+        console.log(e);
+            })
+}
+
+getProducts()
+
+function clickItems() {
+    buttons = document.getElementsByClassName('filter');
+    console.log(buttons);
+    for (button in buttons) {
+        buttons[button].addEventListener("click", filterItems);
+    }
+}
+
+clickItems();
+
+function filterItems(e){
+    console.log(e.target.id);
+    products = document.getElementsByClassName('boxContainer');
+    for (let i = 0; i < products.length; i++) {
+        if (e.target.id == 'men' && products[i].id > 4) {
+            products[i].style.display = 'none'
+            console.log(products[i]);
+        }
+        // else {
+        //     products[i].style.display = 'block';
+        // }
+        if (e.target.id == 'women' && products[i].id < 15) {
+            products[i].style.display = 'none'
+        } 
+        // else {
+        //     // products[i].style.display = 'block'
+        // }
+        if (e.target.id == 'jewelry' && (products[i].id < 5 || products[i].id > 8)) {
+            products[i].style.display = 'none'
+        } 
+        // else {
+        //     products[i].style.display = 'block'
+        // }
+        if (e.target.id == 'electronics' && (products[i].id < 9 || products[i].id > 14)) {
+            products[i].style.display = 'none'
+        } 
+        // else {
+        //     products[i].style.display = 'block'
+        // }
+    }
+}
+    //load products, with an array of just the one's with the ID
+    // loop through the loadproduct array, and only display the filter.
+
+
+function loadProducts(productArr) {
+    const container = document.getElementById("products");
+    for (let i = 0; i < productArr.length; i++) {
+        const boxContainer = document.createElement('div');
+        boxContainer.setAttribute('id', productArr[i].id)
+        boxContainer.classList.add('boxContainer'); 
+        const productName = productArr[i].title;
+        const productPrice = productArr[i].price;
+        const productDescription = productArr[i].description;
+        const productImage = productArr[i].image;
+        const nameBeneath = document.createElement('h6');
+        nameBeneath.innerHTML = productName;
+        boxContainer.appendChild(nameBeneath);
+        // image tag
+        const imageBeneath = document.createElement('img');
+        imageBeneath.src = productImage;
+        boxContainer.appendChild(imageBeneath);
+        const priceBeneath = document.createElement('h5');
+        priceBeneath.innerHTML = `$ ${productPrice}`;
+        boxContainer.appendChild(priceBeneath);
+        const descriptionBeneath = document.createElement('p');
+        descriptionBeneath.innerHTML = productDescription;
+        boxContainer.appendChild(descriptionBeneath);
+        container.appendChild(boxContainer);
+    }
+}
+
+// function for loading categories
+
+// add event listener of each category category 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.use('/',express.static(__dirname+'/product_page/public'));
+app.use('/',express.static(__dirname+'/public/product_page'));
 
 app.listen(5000,()=>{
         console.log('server is running on port 5000');
@@ -14,14 +14,7 @@ app.listen(5000,()=>{
       
 
 app.get('/product_page', (req,res)=>{
-        res.sendFile(__dirname+'/product_page/public/products.html');
-        
-        // console.log(req.headers);
-        // .then(data=>{
-        //   console.log(data);
-        //   res.json(data)
-        // })
-        // .catch(e=>{
-        //   console.log(e);
-        // })
+        res.sendFile(__dirname+'/public/product_page/products.html');      
 })
+
+
