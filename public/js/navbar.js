0c481b80f8cce70982519c589be93017d2ec3e89
shovz <shovz12@gmail.com>
2022-08-07 18:26:57 +0300

    reset

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,33 +1,33 @@
-// let home = document.querySelector(".logo");
-// let searchForm = document.getElementsByTagName("form")[0];
-// let search = document.getElementById("search_Category");
-// let product_search="";
-// home.addEventListener("click",()=>{
-//      window.location.href="http://localhost:3000/"
-//  })
+let home = document.querySelector(".logo");
+let searchForm = document.getElementsByTagName("form")[0];
+let search = document.getElementById("search_Category");
+let product_search="";
+home.addEventListener("click",()=>{
+     window.location.href="http://localhost:3000/"
+ })
 
-//  const getCategory=()=>{
-//     let select_Category = document.getElementById("select_Category").value;
-//     let category = document.getElementById("select_Category")
-//     category.addEventListener('change',(event)=>{
-//         select_Category = event.target.value;
-//     })
-//     return select_Category
-// }
+ const getCategory=()=>{
+    let select_Category = document.getElementById("select_Category").value;
+    let category = document.getElementById("select_Category")
+    category.addEventListener('change',(event)=>{
+        select_Category = event.target.value;
+    })
+    return select_Category
+}
 
-// const searchProduct=(event)=>{
-// event.preventDefault();
-//     let select_Category = getCategory();
-//     localStorage.setItem("search")
-//     window.location.href="http://localhost:3000/search"
+const searchProduct=(event)=>{
+event.preventDefault();
+    let select_Category = getCategory();
+    localStorage.setItem("search")
+    window.location.href="http://localhost:3000/search"
 
-// }
-
-
-
-// search.addEventListener('input',(event)=>{
-// product_search = event.target.value;
+}
 
 
-// searchForm.addEventListener("submit",searchProduct);
-// })
+
+search.addEventListener('input',(event)=>{
+product_search = event.target.value;
+
+
+searchForm.addEventListener("submit",searchProduct);
+})
